8b3edb981787bdd63bb9510c31c9679ebbc743f3
lwt-hhh <2351537149@qq.com>
2019-03-15 17:40:21 +0800

    类和对象

---
+++ c++运算符重载/课堂练习/课堂练习.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+////class lwt
+////{
+////public:
+////	lwt();
+////	lwt(int a, int b=30)
+////	{
+////		x = a;
+////		y = b;
+////	}
+////	int sum()
+////	{
+////		int d = m+x+y;
+////		return d;
+////	}
+////private:
+////	static int m;
+////	int x;
+////	int y;
+////};
+////int lwt::m = 10;
+////
+////int main()
+////{
+////	lwt d1(10);
+////	cout << d1.sum() << endl;
+////	system("pause");
+////	return 0;
+////}
